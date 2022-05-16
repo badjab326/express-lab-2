2c33aa9fab98185f49f02d6a54fd127f0130fa17
“julian <badjab326@gmail.com>
2022-05-16 16:37:24 -0700

    Greeting express application created.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 // 2. Make a route '/greeting'that sends a generic greeting to the screen like "Hello, stranger".
 // 3. Give the greetingroute a param /:name
+// 4. When hitting the route, the page should display a message
 app.get("/greeting/:name", (req, res) => {
     res.send("Hey " + req.params.name + " how's it going?")
 })
